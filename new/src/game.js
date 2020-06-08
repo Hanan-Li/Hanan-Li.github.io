0f76f9dc87941f7d8ca936a6ace6bee6bed8a365
Hanan Li <hananli@umich.edu>
2020-06-08 02:03:11 -0400

    changed instruction line

--- new/src/game.js
+++ new/src/game.js
@@ -27,7 +27,7 @@ const Game = function() {
     bgPlayer15: new Game.BgPlayer(2,1),
 
     title: new Game.Text("Hanan Li", 36*1.3),
-    subtitle: new Game.Text("(ARROW KEYS TO MOVE)", 36*1.8),
+    subtitle: new Game.Text("(TAP SCREEN OR ARROW KEYS TO MOVE)", 36*1.8),
     aboutText: new Game.CollideText("About", 64, 36*2.5, 10,1, "/new/about/"),
     projectText: new Game.CollideText("Projects", 64*2, 36*2.5, 10,1, "/new/projects"),
     resumeText: new Game.CollideText("Résumé", 64*3, 36*2.5, 10,1, "/new/Hanan_Li_Resume_2020.pdf"),
